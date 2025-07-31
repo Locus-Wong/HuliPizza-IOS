57951c6e1c64a669b1c09cfacc09428d6edc2f76
TSZ WA WONG <twwong9@myseneca.ca>
2025-07-31 16:08:31 -0400

    Add order count and remove item button

--- HuliPizza/OrderView.swift
+++ HuliPizza/OrderView.swift
@@ -8,41 +8,52 @@
 import SwiftUI
 
 struct OrderView: View {
-    var orders:[Int]
+    @Binding var orders:[OrderItem]
     var body: some View {
-        ZStack(alignment: .top){
-            ScrollView{
-                ForEach(orders, id: \.self){ order in
-                    OrderRowView(order: order)
-                        .padding(4)
-                        .background(
-                            .regularMaterial,
-                            in: RoundedRectangle(cornerRadius: 10)
-                        )
-                        .shadow(radius: 10)
-                        .padding(.bottom, 5)
-                        .padding([.leading, .trailing], 7)
+        VStack {
+            ZStack(alignment: .top){
+                
+                ScrollView{
+                    ForEach(orders, id: \.id){ order in
+                        Text(order.item.name)
+                        //                    OrderRowView(order: order)
+                            .padding(4)
+                            .background(
+                                .regularMaterial,
+                                in: RoundedRectangle(cornerRadius: 10)
+                            )
+                            .shadow(radius: 10)
+                            .padding(.bottom, 5)
+                            .padding([.leading, .trailing], 7)
+                    }
                 }
+                .padding(.top, 70)
+                HStack {
+                    Text("Order Pizza")
+                        .font(.title)
+                    Spacer()
+                    Label{
+                        Text(59.99, format: .currency(code: "USD"))
+                    }
+                    icon:{
+                        Image(systemName: orders.isEmpty ? "cart" : "cart.fill")
+                    }
+                }.padding()
+                    .background(.ultraThinMaterial)
             }
-            .padding(.top, 70)
-            HStack {
-                Text("Order Pizza")
-                    .font(.title)
-                Spacer()
-                Label{
-                    Text(59.99, format: .currency(code: "USD"))
+            .padding()
+            Button("Delete Order"){
+                if !orders.isEmpty{
+                    orders.removeLast()
                 }
-                icon:{
-                    Image(systemName: orders.isEmpty ? "cart" : "cart.fill")
-                }
-            }.padding()
-                .background(.ultraThinMaterial)
-        }
-        .padding()
-        .background(Color("Surf"))
+            }.padding(5)
+                .background(.regularMaterial, in : Capsule())
+                .padding(7)
+            
+        }.background(Color("Surf"))
     }
 }
 
 #Preview {
-    OrderView(orders: [1,2,3,4,6])
+    OrderView(orders: .constant(testOrders))
 }
