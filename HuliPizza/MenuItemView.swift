5c38b65f1d485a1673fd9d7b9b2b5df1f8ccabcf
TSZ WA WONG <twwong9@myseneca.ca>
2025-07-31 14:58:27 -0400

    Add "addItem" button with icon which is responsive to the number of items in the cart

--- HuliPizza/MenuItemView.swift
+++ HuliPizza/MenuItemView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MenuItemView: View {
+    @State private var addedItem : Bool = false
     var body: some View {
         VStack {
             HStack{
@@ -16,7 +17,7 @@ struct MenuItemView: View {
                     .fontWeight(.semibold)
                     .foregroundStyle(.ultraThickMaterial)
                     .padding(.leading)
-                    
+                
                 if let image = UIImage(named : "0x_lg"){
                     Image(uiImage: image)
                         .resizable()
@@ -24,7 +25,7 @@ struct MenuItemView: View {
                         .padding([.top,.bottom], 5)
                     //                    .clipShape(RoundedRectangle(cornerRadius: 10))
                         .cornerRadius(15)
-                        
+                    
                 } else {
                     Image("surfboard_lg")
                         .resizable()
@@ -39,14 +40,27 @@ struct MenuItemView: View {
                     endPoint: .trailing), in:Capsule()
             ).shadow(color:.teal, radius:5, x:8, y: 8)
             
+            
             VStack(alignment: .leading) {
-                
-                
                 ScrollView {
                     Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at massa et enim volutpat commodo. Sed mattis risus ante, eu suscipit elit auctor ut. Nullam tristique sagittis turpis et dignissim. Proin ullamcorper lacus sit amet nulla porttitor tempus. Donec sed nibh sed dui venenatis gravida. Vestibulum ut ultrices augue. Nunc.")
                         .font(.custom("Georgia", size: 18 , relativeTo: .body))
                 }
+               
             }
+            Button{
+                addedItem.toggle()
+            } label: {
+                Spacer()
+                Text(12.99, format: .currency(code: "USD")).bold()
+                Image(systemName: addedItem ? "cart.badge.plus.fill" : "cart.badge.plus")
+                Spacer()
+            }
+            .padding()
+            .background(.red, in: Capsule())
+            .foregroundStyle(.white)
+            .padding(5)
+          
             
         }
     }
