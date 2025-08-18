43adb244537e636e1b61ccecf016063455362edf
TSZ WA WONG <twwong9@myseneca.ca>
2025-08-18 11:41:02 -0400

    Introduce Conditional ViewBuilder in OrderRowView

--- HuliPizza/OrderRowView.swift
+++ HuliPizza/OrderRowView.swift
@@ -9,18 +9,45 @@ import SwiftUI
 
 struct OrderRowView: View {
     @Binding var order: OrderItem
+    
+    @ViewBuilder var extPrice: some View{
+        Text(order.extPrice, format: .currency(code: "USD"))
+            .fontWeight(.semibold)
+    }
+    
+    @ViewBuilder var pizzaNameView: some View{
+        Text(order.item.name)
+        Spacer()
+        if order.quantity <= 1 {
+            extPrice
+        }
+    }
+    
+    @ViewBuilder var extendedPriceView: some View{
+        if order.quantity > 1 {
+            Text(order.quantity, format: .number)
+            Text(" Pizzas")
+            Spacer()
+            extPrice
+        } else{
+            Text("Single Pizza")
+        }
+    }
+    
     var body: some View {
         VStack {
             HStack {
-                Text(order.item.name)
-                Spacer()
+//                Text(order.item.name)
+//                Spacer()
+                pizzaNameView
             }
             HStack{
-                Text(order.quantity, format: .number)
-                Text(order.item.price, format: .currency(code: "USD"))
-                Spacer()
-                Text(order.extPrice, format: .currency(code: "USD"))
-                    .fontWeight(.semibold)
+//                Text(order.quantity, format: .number)
+//                Text(order.item.price, format: .currency(code: "USD"))
+//                Spacer()
+//                Text(order.extPrice, format: .currency(code: "USD"))
+//                    .fontWeight(.semibold)
+                extendedPriceView
             }
         }
     }
