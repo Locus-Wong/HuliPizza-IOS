470103f9f8890e982d68f22061ae416ec3d2a2a2
TSZ WA WONG <twwong9@myseneca.ca>
2025-08-18 11:29:06 -0400

    Introduced @ViewBuilder and var ratingsView: RatingsView{RatingsView(rating: item.rating)} to clean up the code, and make it much more understandable

--- HuliPizza/MenuRowView.swift
+++ HuliPizza/MenuRowView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct MenuRowView: View {
     var item: MenuItem
+    var ratingsView: RatingsView{RatingsView(rating: item.rating)}
+    @ViewBuilder var namePriceView: some View{
+        Text(item.name)
+        Spacer()
+        Text(item.price, format: .currency(code: "USD"))
+    }
     var body: some View {
         HStack (alignment: .top, spacing: 20){
 //            if let image = UIImage(named: "\(item.id)_sm"){
@@ -18,11 +24,13 @@ struct MenuRowView: View {
 //            }
             VStack (alignment: .leading) {
                 HStack {
-                    Text(item.name)
-                    Spacer()
-                    Text(item.price, format: .currency(code: "USD"))
+//                    Text(item.name)
+//                    Spacer()
+//                    Text(item.price, format: .currency(code: "USD"))
+                    namePriceView
                 }
-                RatingsView(rating: item.rating)
+                //RatingsView(rating: item.rating)
+                ratingsView
             }
         }
     }
